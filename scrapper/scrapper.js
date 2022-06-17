ec76338d2674f2c09e45c8e1fec65b0c4f7c0d46
Leocappiello <leonelcappiello@gmail.com>
2022-06-17 08:57:20 -0300

    Working get the info scrapped ML

--- scrapper/scrapper.js
+++ scrapper/scrapper.js
@@ -11,23 +11,69 @@ const randomUseragent = require('random-useragent');
     page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080 });
     await page.goto(urlFinal)
+
+    //click "nuevo"
+    await page.waitForSelector('a[class~="ui-search-link"][aria-label="Nuevo"]')
+    await page.click('a[class~="ui-search-link"][aria-label="Nuevo"]')
+
+
+    //click desplegable ordenar
+    page.once('load', async () => {
+        await page.waitForSelector('.andes-dropdown__trigger')
+        await page.click('.andes-dropdown__trigger')
+    })
+
+
+    //click "ordenar menor precio"
+    await page.waitForSelector('li[id~="andes-dropdown-más-relevantes-list-option-price_asc"]')
+    await page.click('li[id~="andes-dropdown-más-relevantes-list-option-price_asc"]')
+
+    //seleccionar precios ML
+    //let listItems, listPrecio, listImagen
+    let arrItems = arrPrecio = arrImagen = []
+
+    let getInfo = async () => {
+        let result = page.once('load', async () => {
+            await page.waitForSelector('.ui-search-item__title')
+            listItems = await page.$$('.ui-search-item__title')
+            await page.waitForSelector('.price-tag-fraction')
+            listPrecio = await page.$$('.price-tag-fraction')
+            await page.waitForSelector('.ui-search-result-image__element')
+            listImagen = await page.$$('.ui-search-result-image__element')
+           
+            return listItems
+        })
+
+
+        let options = await page.$$eval('.ui-search-item__title', options => options.map(option => option.textContent))
+
+        return options
+    }
+
+    let listItems = await getInfo()
+    console.log(listItems)
     /*
-    //await page.setUserAgent(header)
-    
- 
-    //await browser.close()
-    
- 
- 
-    await page.waitForSelector('.ui-search-results')
- 
-    const objectNextButton = await page.$('.andes-pagination__button--next a')
- 
-    const getUrl = await page.evaluate(objectNextButton => objectNextButton.getAttribute('href'),
-        objectNextButton);
- 
-    const listaDeItems = await page.$$('.ui-search-layout__item')
+    async function getInnerItems(items){
+        for (let i = 0; i < items.length; i++) {
+            let elem = items[i]
+            let textElem = await page.evaluate(
+                elem => {
+                    elem.textContent
+                    console.log(elem.textContent)
+                },
+                elem
+            )
+            arrItems.push(textElem)
+            //console.log('a')
+        }
+        //return arrItems
+    }
     */
+
+
 })()
+//https://www.youtube.com/watch?v=TDypV9ymmK0
+
+//https://www.youtube.com/playlist?list=PLGreOtbNU07rDURvnQpDaT3XokxlranUQ
 
 //module.exports = initialization
